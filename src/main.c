686f6ad1f8fa32c9e3a9a71992fa664a15ca290b
Arthur Pons <arthur@arthurpons.fr>
2021-03-01 18:46:25 +0100

    Removing error code

--- src/main.c
+++ src/main.c
@@ -12,9 +12,3 @@ Date : 01/03/2021
 int main(void) {
     return EXIT_SUCCESS;
 }
-
-void check_colors(void) {
-    if (has_colors() == FALSE) {
-        printf("Your terminal does not support color\n");
-    }
-}
