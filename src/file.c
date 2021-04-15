859e567dc04833e5db69416afba393d57273ac72
Arthur Pons <arthur@arthurpons.fr>
2021-04-15 19:03:18 +0200

    Merge conflict resolved

--- src/file.c
+++ src/file.c
@@ -25,17 +25,8 @@ void writeToFile(const char *filename, const char *content) {
 
 void readFile(const char*filename, char linesBuf[128][391], int *hostNumber) {
     FILE *f = fopen(filename, "rb");
-<<<<<<< HEAD
     int i = 0;
     while (fgets(linesBuf[i++], 391, f));
-    *hostNumber = i;
+    *hostNumber = i;s
     fclose(f);
-=======
-    if(f != NULL) {
-        int i = 0;
-        while (fgets(linesBuf[i++], 391, f));
-        *hostNumber = i;
-    }
-
->>>>>>> ed7552587e8b97cb51015a82385fbf19b4f53a31
 }
